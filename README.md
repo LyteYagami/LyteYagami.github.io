6423c41cef4082295613492c241fc7ae980a0ed1
Lyte_Yagami <nuh.satria@outlook.com>
2019-08-17 11:14:01 +0800

    replaced file
    replaced files with files for robofriends instead

--- README.md
+++
@@ -1,4 +0,0 @@
-# LyteYagami.github.io
-Dark Abstract Art gallery
-
-This is all about Dark Art pics that favours my taste lol
---
+++ precache-manifest.988cc2a8aa8a83ff6a7e578ef0ccde77.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0896d27bf18ce8577ce644ddd7f1e79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "869d687ba3799ca828c0",
+    "url": "/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "9b9ed1e375f05b47822a",
+    "url": "/static/css/main.d97c9d1c.chunk.css"
+  },
+  {
+    "revision": "869d687ba3799ca828c0",
+    "url": "/static/js/2.29f1c20e.chunk.js"
+  },
+  {
+    "revision": "9b9ed1e375f05b47822a",
+    "url": "/static/js/main.9065de81.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "131a602978939eac179588732766635f",
+    "url": "/static/media/CLIFF.131a6029.woff"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "68db9b846d69a5d75aed18d08c698d4b",
+    "url": "/static/media/galaxy_1.68db9b84.woff"
+  }
+]);
